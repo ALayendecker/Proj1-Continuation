1af1a867ba450a14340040cb8a841a4975792e91
ALayendecker <alcommits@gmail.com>
2019-10-07 12:57:29 -0500

    working stylechanger.js and styling for dropdown class for stylesheet index

---
+++ assets/javascript/stylechanger.js
@@ -0,0 +1,25 @@
+$("#creative").click(function() {
+  $(".style-sheets").attr("href", "assets/css/creative.css");
+});
+$("#bummed").click(function() {
+  $(".style-sheets").attr("href", "assets/css/sleepy.css");
+});
+$("#chill").click(function() {
+  $(".style-sheets").attr("href", "assets/css/sleepy.css");
+});
+$("#party").click(function() {
+  $(".style-sheets").attr("href", "assets/css/mad.css");
+});
+$("#working").click(function() {
+  $(".style-sheets").attr("href", "assets/css/motivated.css");
+});
+$("#feeling-good").click(function() {
+  $(".style-sheets").attr("href", "assets/css/styles.css");
+});
+$("#romantic").click(function() {
+  $(".style-sheets").attr("href", "assets/css/mad.css");
+});
+$("#lonely").click(function() {
+  $(".style-sheets").attr("href", "assets/css/sleepy.css");
+});
+console.log("hello world");
