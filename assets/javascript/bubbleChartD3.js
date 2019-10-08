070b7d0e4d3b7400336e8c0da2b278e644ef46a1
ALayendecker <alcommits@gmail.com>
2019-10-08 13:17:29 -0500

    bubble chart with logic and most updated index

--- assets/javascript/bubbleChartD3.js
+++ assets/javascript/bubbleChartD3.js
@@ -1,284 +1,128 @@
-//firebase config
-var firebaseConfig = {
-  apiKey: "AIzaSyC4Rc8sreGaXTBDOt7SZ0MqqUab3uW8D0s",
-  authDomain: "peppy-sensor-255201.firebaseapp.com",
-  databaseURL: "https://peppy-sensor-255201.firebaseio.com",
-  projectId: "peppy-sensor-255201",
-  storageBucket: "",
-  messagingSenderId: "924132566669",
-  appId: "1:924132566669:web:35902be7f88eb27c3aa4c4"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-//----------------------------
-//clicker activity code
-//----------------------------
-var database = firebase.database();
-
-// var creativeCounter = 0;
-// var bummedCounter = 0;
-// var chillCounter = 0;
-// var partyCounter = 0;
-// var workingCounter = 0;
-// var feelingGoodCounter = 0;
-// var romanticalCounter = 0;
-// var lonelyCounter = 0;
-
-// console.log(creativeCounter);
-// database.ref().set({
-//   creativeCount: creativeCounter,
-//   bummedCount: bummedCounter,
-//   chillCount: chillCounter,
-//   partyCount: partyCounter,
-//   workingCount: workingCounter,
-//   feelingGoodCount: feelingGoodCounter,
-//   romanticalCount: romanticalCounter,
-//   lonelyCount: lonelyCounter
-// });
-// function databaseCounter() {
-// Setting initial value of our click counter variable to 0
-
-// FUNCTIONS + EVENTS
-// --------------------------------------------------------------------------------
-
-// On Click of Button
-
-$(".dropdown-item").on("click", function() {
-  console.log($(this).text());
-});
-//   if ("#creative") function(){
-//     creativeCounter++
-//     database.ref().set({
-//       creativeCount: creativeCounter
-//     });
-//   }
-// )}
-// $("#creative").on("click", function() {
-//   // Add to creativeCounter
-//   creativeCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     creativeCount: creativeCounter
-//   });
-// });
-// $("#bummed").on("click", function() {
-//   // Add to bummedCounter
-//   bummedCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     bummedCount: bummedCounter
-//   });
-// });
-// $("#chill").on("click", function() {
-//   // Add to chillCounter
-//   chillCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     chillCount: chillCounter
-//   });
-// });
-// $("#party").on("click", function() {
-//   // Add to partyCounter
-//   partyCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     partyCount: partyCounter
-//   });
-// });
-// $("#working").on("click", function() {
-//   // Add to workingCounter
-//   workingCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     workingCount: workingCounter
-//   });
-// });
-// $("#feeling-good").on("click", function() {
-//   // Add to feelingGoodCounter
-//   feelingGoodCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     feelingGoodCount: feelingGoodCounter
-//   });
-// });
-// $("#romantic").on("click", function() {
-//   // Add to romanticalCounter
-//   romanticalCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     romanticalCount: romanticalCounter
-//   });
-// });
-// $("#lonely").on("click", function() {
-//   // Add to lonelyCounter
-//   lonelyCounter++;
-//   console.log(this);
-
-//   database.ref().set({
-//     lonelyCount: lonelyCounter
-//   });
-// });
-
-// MAIN PROCESS + INITIAL CODE
-// --------------------------------------------------------------------------------
-
-// Using .on("value", function(snapshot)) syntax will retrieve the data
-// from the database (both initially and every time something changes)
-// This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
-database.ref().on(
-  "value",
-  function(snapshot) {
-    // Then we console.log the value of snapshot
-    console.log(snapshot.val().creativeCount);
-    console.log(snapshot.val().bummedCount);
-    console.log(snapshot.val().chillCount);
-    console.log(snapshot.val().partyCount);
-    console.log(snapshot.val().workingCount);
-    console.log(snapshot.val().feelingGoodCount);
-    console.log(snapshot.val().romanticalCount);
-    console.log(snapshot.val().lonelyCount);
-
-    // Update the counter variable with data from the database.
-    creativeCounter = snapshot.val().creativeCount;
-    bummedCounter = snapshot.val().bummedCount;
-    chillCounter = snapshot.val().chillCount;
-    partyCounter = snapshot.val().partyCount;
-    workingCounter = snapshot.val().workingCount;
-    feelingGoodCounter = snapshot.val().feelingGoodCount;
-    romanticalCounter = snapshot.val().romanticalCount;
-    lonelyCounter = snapshot.val().lonelyCount;
-    // Then we change the html associated with the number.
-
-    // the #creative-value stuff needs to be going into d3 not into
-    // $("#creative-value").text(snapshot.val().creativeCount);
-    // $("#creative-value").text(snapshot.val().bummedCount);
-    // $("#creative-value").text(snapshot.val().chillCount);
-    // $("#creative-value").text(snapshot.val().partyCount);
-    // $("#creative-value").text(snapshot.val().workingCount);
-    // $("#creative-value").text(snapshot.val().feelingCount);
-    // $("#creative-value").text(snapshot.val().romanticalCount);
-    // $("#creative-value").text(snapshot.val().lonelyCount);
-
-    // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-    // Again we could have named errorObject anything we wanted.
-  },
-  function(errorObject) {
-    // In case of error this will print the error
-    console.log("The read failed: " + errorObject.code);
-  }
-);
 // $(document).ready(function() {
-//   $(document).on("click", ".dropdown-item", databaseCounter);
+// var clickCount = localStorage.getItem("clickCount");
+var creativeCounter = localStorage.getItem("creativeCount");
+var bummedCounter = localStorage.getItem("bummedCount");
+var chillCounter = localStorage.getItem("chillCount");
+var partyCounter = localStorage.getItem("partyCount");
+var workingCounter = localStorage.getItem("workingCount");
+var feelingGoodCounter = localStorage.getItem("feelingGoodCount");
+var romanticalCounter = localStorage.getItem("romanticalCount");
+var lonelyCounter = localStorage.getItem("lonelyCount");
+
+// creativeCounter = creativeCounter ? parseInt(creativeCounter) : 0;
+// bummedCounter = bummedCounter ? parseInt(bummedCounter) : 0;
+// chillCounter = chillCounter ? parseInt(chillCounter) : 0;
+// partyCounter = partyCounter ? parseInt(partyCounter) : 0;
+// workingCounter = workingCounter ? parseInt(workingCounter) : 0;
+// feelingGoodCounter = feelingGoodCounter ? parseInt(feelingGoodCounter) : 0;
+// romanticalCounter = romanticalCounter ? parseInt(romanticalCounter) : 0;
+// lonelyCounter = lonelyCounter ? parseInt(lonelyCounter) : 0;
+
+// $(".dropdown-item").click(function() {
+$("#creative").click(function() {
+  ++creativeCounter;
+});
+console.log("creative" + creativeCounter);
+$("#bummed").click(function() {
+  ++bummedCounter;
+});
+console.log("bummed" + bummedCounter);
+$("#chill").click(function() {
+  ++chillCounter;
+});
+console.log("chill" + chillCounter);
+$("#party").click(function() {
+  ++partyCounter;
+});
+console.log("part" + partyCounter);
+$("#working").click(function() {
+  ++workingCounter;
+});
+console.log("working" + workingCounter);
+$("#feeling-good").click(function() {
+  ++feelingGoodCounter;
+});
+console.log("feeling" + feelingGoodCounter);
+$("#romantic").click(function() {
+  ++romanticalCounter;
+});
+console.log("rom" + romanticalCounter);
+$("#lonely").click(function() {
+  ++lonelyCounter;
+});
+console.log("lonely" + lonelyCounter);
+
+// $(".dropdown-item").on("click", counter);
+// $("#creative").on("click", counter);
+// $("#bummed").on("click", counter);
+// $("#chill").on("click", counter);
+// $("#party").on("click", counter);
+// $("#working").on("click", counter);
+// $("#feeling-good").on("click", counter);
+// $("#romantic").on("click", counter);
+// $("#lonely").on("click", counter);
+
+localStorage.setItem("creativeCount", creativeCounter);
+localStorage.setItem("bummedCount", bummedCounter);
+localStorage.setItem("chillCount", chillCounter);
+localStorage.setItem("partyCount", partyCounter);
+localStorage.setItem("workingCount", workingCounter);
+localStorage.setItem("feelingGoodCount", feelingGoodCounter);
+localStorage.setItem("romanticalCount", romanticalCounter);
+localStorage.setItem("lonelyCount", lonelyCounter);
 // });
-//----------------------------
-//D3 Code
-//----------------------------
+// });
 
-// dataset = {
-//   children: [
-//     { Name: "Creative", Count: 0 },
-//     { Name: "Bummed", Count: 0 },
-//     { Name: "Chill", Count: 0 },
-//     { Name: "Party", Count: 0 },
-//     { Name: "Working", Count: 0 },
-//     { Name: "Feeling Good", Count: 0 },
-//     { Name: "Rommantical", Count: 0 },
-//     { Name: "Lonely", Count: 0 }
-//   ]
-// };
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: [
+      "Creative",
+      "Bummed",
+      "Chill",
+      "Party",
+      "Working",
+      "Feeling Good",
+      "Romantic",
+      "Lonely"
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [
+          creativeCounter,
+          bummedCounter,
+          chillCounter,
+          partyCounter,
+          workingCounter,
+          feelingGoodCounter,
+          romanticalCounter,
+          lonelyCounter
+        ],
+        // data: [1, 2, 3, 4, 5, 6, 7, 8],
 
-// // dataset = {
-// //   children: [
-// //     { Name: "Creative", Count: snapshot.val().creativeCount },
-// //     { Name: "Bummed", Count: snapshot.val().bummedCount },
-// //     { Name: "Chill", Count: snapshot.val().chillCount },
-// //     { Name: "Party", Count: snapshot.val().partyCount },
-// //     { Name: "Working", Count: snapshot.val().workingCount },
-// //     { Name: "Feeling Good", Count: snapshot.val().feelingGoodCout },
-// //     { Name: "Rommantical", Count: snapshot.val().romanticalCount },
-// //     { Name: "Lonely", Count: snapshot.val().lonelyCount }
-// //   ]
-// // };
-// function createChart() {
-//   var diameter = 600;
-//   var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-//   var bubble = d3
-//     .pack(dataset)
-//     .size([diameter, diameter])
-//     .padding(1.5);
-
-//   var svg = d3
-//     .select("body")
-//     .append("svg")
-//     .attr("width", diameter)
-//     .attr("height", diameter)
-//     .attr("class", "bubble");
-
-//   var nodes = d3.hierarchy(dataset).sum(function(d) {
-//     return d.Count;
-//   });
-
-//   var node = svg
-//     .selectAll(".node")
-//     .data(bubble(nodes).descendants())
-//     .enter()
-//     .filter(function(d) {
-//       return !d.children;
-//     })
-//     .append("g")
-//     .attr("class", "node")
-//     .attr("transform", function(d) {
-//       return "translate(" + d.x + "," + d.y + ")";
-//     });
-
-//   node.append("title").text(function(d) {
-//     return d.Name + ": " + d.Count;
-//   });
-
-//   node
-//     .append("circle")
-//     .attr("r", function(d) {
-//       return d.r;
-//     })
-//     .style("fill", function(d, i) {
-//       return color(i);
-//     });
-
-//   node
-//     .append("text")
-//     .attr("dy", ".2em")
-//     .style("text-anchor", "middle")
-//     .text(function(d) {
-//       return d.data.Name.substring(0, d.r / 3);
-//     })
-//     .attr("font-family", "Days One", "sans-serif")
-//     .attr("font-size", function(d) {
-//       return d.r / 5;
-//     })
-//     .attr("fill", "white");
-
-//   node
-//     .append("text")
-//     .attr("dy", "1.3em")
-//     .style("text-anchor", "middle")
-//     .text(function(d) {
-//       return d.data.Count;
-//     })
-//     .attr("font-family", "Days One", "sans-serif")
-//     .attr("font-size", function(d) {
-//       return d.r / 5;
-//     })
-//     .attr("fill", "white");
-
-//   d3.select(self.frameElement).style("height", diameter + "px");
-// }
-// //modified from Alok K. Shukla bubble chart
-// //found https://bl.ocks.org/alokkshukla/3d6be4be0ef9f6977ec6718b2916d168
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ]
+        // borderColor: [
+        //   "rgba(255, 99, 132, 1)",
+        //   "rgba(54, 162, 235, 1)",
+        //   "rgba(255, 206, 86, 1)",
+        //   "rgba(75, 192, 192, 1)",
+        //   "rgba(153, 102, 255, 1)",
+        //   "rgba(255, 159, 64, 1)"
+        // ],
+        // borderWidth: 1
+      }
+    ]
+  },
+  options: {}
+});
