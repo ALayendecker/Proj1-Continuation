193eb03296d1aaf40d9ea5be56ed6bbc1ac9b120
ALayendecker <alcommits@gmail.com>
2019-10-08 21:34:38 -0500

    fixed bug preventing api calls on two dropdown items

--- assets/javascript/quote.js
+++ assets/javascript/quote.js
@@ -1,64 +1,59 @@
-
 $(".dropdown-item").on("click", function() {
-    var searchTerm;
-    var moodQuote = $(this).attr("id");
-    console.log(moodQuote);
-    if (moodQuote === "creative") {
-        searchTerm = "love";
-    }
-    if   (moodQuote === "bummed") {
-          searchTerm = "motivational";
-        }
-        if  (moodQuote === "chill") {
-            searchTerm = "humor";
-          }
-          if   (moodQuote === "party") {
-            searchTerm = "happiness";
-          }
-          if   (moodQuote === "party") {
-            searchTerm = "happiness";
-          }
-          if   (moodQuote === "working") {
-            searchTerm = "wisdom";
-          }
-          if   (moodQuote === "feeling good") {
-            searchTerm = "success";
-          }
-          if   (moodQuote === "romantical") {
-            searchTerm = "romance";
-          }
-          if   (moodQuote === "lonely") {
-            searchTerm = "death";
-          }
+  var searchTerm;
+  var moodQuote = $(this).attr("id");
+  console.log(moodQuote);
+  if (moodQuote === "creative") {
+    searchTerm = "love";
+  }
+  if (moodQuote === "bummed") {
+    searchTerm = "motivational";
+  }
+  if (moodQuote === "chill") {
+    searchTerm = "humor";
+  }
+  if (moodQuote === "party") {
+    searchTerm = "happiness";
+  }
+  if (moodQuote === "party") {
+    searchTerm = "happiness";
+  }
+  if (moodQuote === "working") {
+    searchTerm = "wisdom";
+  }
+  if (moodQuote === "feeling-good") {
+    searchTerm = "success";
+  }
+  if (moodQuote === "romantic") {
+    searchTerm = "romance";
+  }
+  if (moodQuote === "lonely") {
+    searchTerm = "death";
+  }
 
-    
-
-    var queryURL = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://goodreads-best-quotes.p.rapidapi.com/random/" + searchTerm,
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "goodreads-best-quotes.p.rapidapi.com",
-            "x-rapidapi-key": "b530af42f2mshe5e195db966c7cfp1b3a00jsncc9c887382eb"
-        }
+  var queryURL = {
+    async: true,
+    crossDomain: true,
+    url: "https://goodreads-best-quotes.p.rapidapi.com/random/" + searchTerm,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "goodreads-best-quotes.p.rapidapi.com",
+      "x-rapidapi-key": "b530af42f2mshe5e195db966c7cfp1b3a00jsncc9c887382eb"
     }
-    
-    $.ajax(queryURL).done(function (response) {
-        console.log("Call response: " + response);
-        response =JSON.parse(response);
-        var targetDiv = document.getElementById("quote");
-        
-        targetDiv.textContent = response.data.content;
-        
-        var newDiv = document.createElement("div");
-        newDiv.textContent = response.data.author[0];
-        
-        targetDiv.appendChild(newDiv);
-    });
+  };
+
+  $.ajax(queryURL).done(function(response) {
+    console.log("Call response: " + response);
+    response = JSON.parse(response);
+    var targetDiv = document.getElementById("quote");
+
+    targetDiv.textContent = response.data.content;
+
+    var newDiv = document.createElement("div");
+    newDiv.textContent = response.data.author[0];
+
+    targetDiv.appendChild(newDiv);
+  });
 });
 
-    // We then apply that CSS to our newDiv.
-    // newDiv.setAttribute("class", "fancy");
-
-  
+// We then apply that CSS to our newDiv.
+// newDiv.setAttribute("class", "fancy");
